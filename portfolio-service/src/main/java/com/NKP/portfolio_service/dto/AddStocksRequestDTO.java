be470eb8490613a964e1cdfb1afbfdfe4f2fa922
Navaneeth Krishna P <navneeth10p@gmail.com>
2026-04-03 17:36:52 +0530

    added validation

--- portfolio-service/src/main/java/com/NKP/portfolio_service/dto/AddStocksRequestDTO.java
+++ portfolio-service/src/main/java/com/NKP/portfolio_service/dto/AddStocksRequestDTO.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -11,9 +14,14 @@ import java.math.BigDecimal;
 @Data
 public class AddStocksRequestDTO {
 
-    private long userId;
+    @NotNull
+    private Long userId;
+    @NotBlank
     private String stockName;
+    @NotBlank
     private String symbol;
+    @Min(value = 1,message = "Quantity must be equal to or more than 1")
     private long quantities;
+    @NotBlank
     private BigDecimal orderPrice;
 }
