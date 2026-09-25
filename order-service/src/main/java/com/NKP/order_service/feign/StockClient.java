0d5e3f9885da7c5c469ecd18c7becb6fcbf5e7fa
Navaneeth Krishna P <navneeth10p@gmail.com>
2026-09-25 13:10:41 +0530

    Externalised service name to app.yml file in order-service

--- order-service/src/main/java/com/NKP/order_service/feign/StockClient.java
+++ order-service/src/main/java/com/NKP/order_service/feign/StockClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("STOCK-SERVICE")
+@FeignClient("${stock.service.name}")
 public interface StockClient {
 
     @GetMapping("/stocks/{symbol}")
